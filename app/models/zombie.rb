a2b195a91689713151e12d7f179030b6aa545036
Arlan Uy <gnhs.com@gmail.com>
2016-10-02 17:08:01 +0800

    make rotting method done

--- app/models/zombie.rb
+++ app/models/zombie.rb
@@ -1,2 +1,12 @@
 class Zombie < ActiveRecord::Base
+  scope :rotting, where(rotting: true)
+  scope :fresh, where("age <20")
+  scope :recent, order("created_at_desc").limit(3)
+
+  before_save :make_rotting
+
+  def make_roting
+    self.rotting = true if age > 20
+    end
+  end
 end
